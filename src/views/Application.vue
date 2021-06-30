2266e5cbbca299651665588249c6365c395c928d
Maxime Moreillon <m.moreillon@gmail.com>
2021-06-30 13:55:22 +0900

    help

--- src/views/Application.vue
+++ src/views/Application.vue
@@ -13,7 +13,7 @@
 
         <v-dialog
           v-model="help_dialog"
-          width="500">
+          width="800">
 
           <template v-slot:activator="{ on, attrs }">
             <v-btn
@@ -28,13 +28,51 @@
 
 
           <v-card>
-            <v-card-title class="text-h5">
-              Help
-            </v-card-title>
+            <v-card-title>Help</v-card-title>
 
+            <v-card-subtitle class="mt-4">機能</v-card-subtitle>
+            <v-card-text>添付のPDFに捺印（サイズ変更も可能）が可能です。</v-card-text>
+
+            <v-card-subtitle>回付手順</v-card-subtitle>
             <v-card-text>
-              1. Click the pdf
+              <v-list dense>
+                <v-list-item>
+                  <v-list-item-content>
+                    １．ページ下部に表示されているPDF上の任意の場所に捺印します。（サイズ変更可能）
+                  </v-list-item-content>
+                </v-list-item>
+                <v-list-item>
+                  <v-list-item-content>
+                    ２．メール作成ボタンを押すと、自動で次の回付者へメールが作成されます。
+                  </v-list-item-content>
+                </v-list-item>
+                <v-list-item>
+                  <v-list-item-content>
+                    ３．作成されたメール（Outlook）をご自身で送信して下さい。
+                  </v-list-item-content>
+                </v-list-item>
+                <v-list-item>
+                  <v-list-item-content>
+                    ４．以上で回付作業は終了です。ブラウザの×のボタンで閉じて下さい。
+                  </v-list-item-content>
+                </v-list-item>
+              </v-list>
+
             </v-card-text>
+
+            <v-card-subtitle>捺印済みファイルのダウンロード手順</v-card-subtitle>
+            <v-card-text>ページに表示されているPDF上部にあるダウンロードから捺印済みのファイルをダウンロードします。</v-card-text>
+
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                color="#c00000"
+                text
+                @click="help_dialog = false">
+                Close
+              </v-btn>
+            </v-card-actions>
+
 
 
 
