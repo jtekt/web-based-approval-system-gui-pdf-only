d22c72b3c45000b6431eb892525b58e1da7d161a
Maxime Moreillon <m.moreillon@gmail.com>
2022-03-02 15:51:09 +0900

    multiline comments

---
+++ src/components/RecipientComment.vue
@@ -0,0 +1,161 @@
+<template>
+
+  <v-list-item
+    two-line>
+
+    <v-list-item-content>
+      <v-list-item-subtitle>{{recipient.properties.display_name}}</v-list-item-subtitle>
+      <!-- <v-list-item-title v-if="!editing">
+        {{recipient_comment || 'コメント無し / No comment'}}
+      </v-list-item-title> -->
+      <v-list-item-title>
+        <v-textarea
+          v-if="editing"
+          outlined
+          auto-grow
+          dense
+          :rows="1"
+          v-model="new_comment" />
+        <v-textarea
+          v-else
+          solo
+          flat
+          readonly
+          auto-grow
+          dense
+          :rows="1"
+          :value="recipient_comment" />
+      </v-list-item-title>
+    </v-list-item-content>
+
+    <v-list-item-icon
+      v-if="recipient_is_user">
+      <template v-if="editing">
+        <v-btn
+          @click="disable_editing()"
+          icon>
+          <v-icon>mdi-close</v-icon>
+        </v-btn>
+        <v-btn
+          @click="update_comment()"
+          icon>
+          <v-icon>mdi-content-save</v-icon>
+        </v-btn>
+      </template>
+
+      <v-btn
+        v-if="!editing"
+        @click="enable_editing()"
+        icon>
+        <v-icon>mdi-pencil</v-icon>
+      </v-btn>
+    </v-list-item-icon>
+
+  </v-list-item>
+
+  <!-- <tr>
+
+
+    <td>
+      <textarea
+        v-if="editing && decision"
+        v-model="new_comment"/>
+      <div
+        v-else
+        style="white-space: pre-line;" v-text="recipient_comment || 'No comment'" />
+
+    </td>
+    <td class="actions">
+      <button
+        type="button"
+        @click="enable_editing()"
+        v-if="recipient_is_user && !editing">
+        <pencil-icon/>
+      </button>
+      <button
+        type="button"
+        @click="disble_editing()"
+        v-if="editing">
+        <close-icon/>
+      </button>
+      <button
+        type="button"
+        @click="update_comment()"
+        v-if="editing">
+        <content-save-icon/>
+      </button>
+
+    </td>
+  </tr> -->
+</template>
+
+<script>
+import IdUtils from '@/mixins/IdUtils.js'
+
+export default {
+  name: 'ApprovalComment',
+  props: {
+    recipient: Object,
+  },
+  mixins: [
+    IdUtils
+  ],
+  data(){
+    return {
+      editing: false,
+      new_comment: '',
+    }
+  },
+  methods: {
+    enable_editing(){
+      if (!this.recipient_is_user) return
+      this.new_comment = this.recipient_comment || ''
+      this.editing = true
+    },
+    disable_editing(){
+      this.editing = false
+    },
+    update_comment () {
+
+      if (!this.recipient_is_user) return
+      const decision_id = this.get_id_of_item(this.decision)
+      const url = `${process.env.VUE_APP_SHINSEI_MANAGER_URL}/decisions/${decision_id}/comment`
+      const body = { comment: this.new_comment || "No comment" }
+      this.axios.put(url, body)
+        .then(() => {
+          this.disable_editing()
+          this.$emit('comment_updated')
+        })
+        .catch(error => {
+          alert('Edit failed')
+          if(error.response) console.error(error.response.data)
+          else console.error(error)
+        })
+    },
+
+
+
+
+  },
+  computed: {
+    recipient_is_user () {
+      return this.get_id_of_item(this.recipient) === this.current_user_id
+    },
+    decision(){
+      return this.recipient.approval || this.recipient.refusal
+    },
+
+    recipient_comment(){
+      if(!this.decision) return null
+      return this.decision.properties.comment || 'コメント無し / No comment'
+
+    },
+  }
+
+}
+</script>
+
+<style scoped>
+
+
+</style>
