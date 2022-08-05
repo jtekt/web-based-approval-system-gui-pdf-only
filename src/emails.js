f29b264390518f469bda4ad7f376ad8e5169e7f1
Maxime Moreillon <m.moreillon@gmail.com>
2022-08-06 07:43:02 +0900

    cleanued up emails

--- src/emails.js
+++ src/emails.js
@@ -10,7 +10,7 @@ const generate_email_to_recipient_body = (application, recipient) =>
 申請を提出しました。
 
 申請者: ${application.applicant.display_name}
-タイトル: ${application.title}
+件名: ${application.title}
 提出先URL: ${window.location.origin}/applications/${application._id}
 
 確認お願いします。`
@@ -28,7 +28,7 @@ const generate_email_to_applicant_body = (application) =>
 申請の承認が${application_is_rejected(application) ? '却下' : '完了'}されました。
 
 申請者: ${application.applicant.display_name}
-タイトル: ${application.title}
+件名: ${application.title}
 提出先URL: ${window.location.origin}/applications/${application._id}
 
 確認お願いします。`
