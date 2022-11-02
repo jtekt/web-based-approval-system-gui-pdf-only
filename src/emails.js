ec703c1bd34f5733ae20f63d9d4b3816e31f15a9
Uchino <00182274@jtekt.co.jp>
2022-11-02 14:52:16 +0900

    add comment to body and add rejected to subject

--- src/emails.js
+++ src/emails.js
@@ -4,7 +4,15 @@ const application_is_rejected = (application) => {
 
 const generate_email_to_recipient_body = (application, recipient) => {
 
-    // const applicant_comment = application.form_data[1].value
+    let applicant_comment = application.form_data[1].value
+    let application_title = application.title
+
+    if ((application_title.length + applicant_comment.length) > 50) {
+        applicant_comment = application.form_data[1].value.replace(/\r?\n/g, '').substring( 0, 30)
+        applicant_comment = applicant_comment + "(以下省略)"
+        application_title = application.title.substring( 0, 20)
+        application_title = application_title + "(以下省略)"
+    }
 
     return `${recipient.display_name} 様
 
@@ -13,8 +21,9 @@ const generate_email_to_recipient_body = (application, recipient) => {
 申請を提出しました。
 
 申請者: ${application.applicant.display_name}
-件名: ${application.title}
+件名: ${application_title}
 提出先URL: ${window.location.origin}/applications/${application._id}
+申請者コメント: ${applicant_comment}
 
 確認お願いします。`
 }
@@ -40,7 +49,7 @@ const generate_email_to_applicant_body = (application) =>
 
 const generate_email_to_applicant = (application) =>
     `mailto:${application.applicant.email_address}
-?subject=[電子捺印システム] ${application.title}
+?subject=[電子捺印システム] ${application_is_rejected(application) ? '[却下]' : '[承認]'} ${application.title}
 &body=${encodeURIComponent(generate_email_to_applicant_body(application))}`
 
 export {
